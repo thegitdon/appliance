16fa11689d588cbd41dd9092dfafcd989ff95fe6
Raul Andres Hernandez <raulandres462@gmail.com>
2026-08-08 12:40:28 -0500

    Se corrigió bug en dropdowns de menú

--- src/components/common/navbar/NavbarMenu.vue
+++ src/components/common/navbar/NavbarMenu.vue
@@ -13,8 +13,10 @@ defineProps<{
         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
           {{ item.label }}
         </button>
-        <ul class="dropdown-menu" v-for="child in item.children">
-          <li><a class="dropdown-item" :href="child.to">{{ child.label }}</a></li>
+        <ul class="dropdown-menu">
+          <li v-for="child in item.children">
+            <a class="dropdown-item" :href="child.to">{{ child.label }}</a>
+          </li>
         </ul>
       </div>
     </div>
