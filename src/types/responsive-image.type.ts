85fa99708aff2018f1b291670bf6fd01b8497f9e
Raul Andres Hernandez <raulandres462@gmail.com>
2026-08-21 19:24:03 -0500

    Se simplificaron interfaces de Responsive image

--- src/types/responsive-image.type.ts
+++ src/types/responsive-image.type.ts
@@ -1,27 +1,11 @@
-export type ImageSize = 'small' | 'medium' | 'large'
+export type ImageSize = 'small' | 'medium' | 'large';
+
+export type ImageRegistry = ReadonlyArray<Record<string, ResponsiveImageType>>;
 
 export interface ResponsiveImageType {
     multiples: boolean
-    image?: unknown
-    images?: unknown
+    image?: string,
+    images?: Record<ImageSize, string>;
     alt: string;
     title: string;
 }
-
-export interface SingleImageConfig {
-    multiples: false;
-    image: string;
-    alt?: string;
-    title?: string;
-}
-
-export interface MultipleImageConfig {
-    multiples: true;
-    images: Record<ImageSize, string>;
-    alt?: string;
-    title?: string;
-}
-
-export type ImageConfig = SingleImageConfig | MultipleImageConfig
-
-export type ImageRegistry = ReadonlyArray<Record<string, ResponsiveImageType>>
