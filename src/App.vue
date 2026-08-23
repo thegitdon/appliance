3d068cc27cc7907c9477150b00e17f5a1cf82ada
Raul Andres Hernandez <raulandres462@gmail.com>
2026-08-22 23:48:14 -0500

    Se creó custom container

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Footer from './components/common/footer/Footer.vue';
 </script>
 
 <template>
-  <div class="container">
+  <div class="custom-container">
     <AppNavbar />
 
     <RouterView />
--- src/components/common/navbar/AppNavbar.vue
+++ src/components/common/navbar/AppNavbar.vue
@@ -28,7 +28,7 @@ onMounted(async () => {
 </script>
 
 <template>
-    <header class="app-navbar">
+    <header class="app-navbar rounded-3 mt-3">
         <NavbarTop :social-links="socialLinks" />
         <NavbarMenu :items="navbarMenuItems" />
     </header>
@@ -38,5 +38,6 @@ onMounted(async () => {
 .app-navbar {
     background-color: var(--color-surface);
     color: var(--color-text);
+    margin: 0 1rem;
 }
 </style>
